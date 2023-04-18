c5299fb5055fa26be3813e2f2174fac1708f50a8
Dubem Otekeiwebia <dondubbie007@gmail.com>
2023-04-18 13:00:54 +0100

    Add project files.

---
+++ SignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductChat.API.Models;
+
+namespace ProductChat.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task RecieveProduct(Product p);
+        
+    }
+}
---
+++ SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using ProductChat.API.Models;
+
+namespace ProductChat.API.Hubs
+{
+    public class ProductHub:Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.RecieveProduct(p);
+        }
+    }
+}
